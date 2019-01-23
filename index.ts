a32675eedaf598c15023a0a1d6f2dd8cff15b342
keenondrums <andrey.goncharov+it@protonmail.com>
2019-01-23 21:08:51 +0300

    Add test for ReducerClass

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
 export { Action } from './src/decorator-action'
 export { METADATA_KEY_ACTION } from './src/constants'
-export { MetadataActionMissingError } from './src/errors'
+export * from './src/errors'
 export { ReducerClass } from './src/reducer-class'
--- src/reducer-class.test.ts
+++ src/reducer-class.test.ts
@@ -1,23 +1,40 @@
 // tslint:disable max-classes-per-file
 import 'reflect-metadata'
 
-// import { ActionStandard } from 'flux-action-class'
+import { ActionStandard } from 'flux-action-class'
 
-// import { Action } from './decorator-action'
+import { Action } from './decorator-action'
 import { ReducerClass } from './reducer-class'
 
 describe(ReducerClass.name, () => {
   test('creates a reducer', () => {
-    // class Action1 extends ActionStandard {}
-    // class Action2 extends ActionStandard<number> {}
-    // class Test extends ReducerClass<number> {
-    //   public initialState = 0
-    //   @Action(Action1)
-    //   public test1(state: number, action: any) {
-    //     return 2
-    //   }
-    //   @Action(Action2)
-    //   public test2(state: number, draft: number, action: any) {} // tslint:disable-line no-empty
-    // }
+    interface ITestState {
+      sum: number
+    }
+    class Action1 extends ActionStandard {}
+    class Action2 extends ActionStandard<number> {}
+    class Test extends ReducerClass<ITestState> {
+      public initialState = { sum: 10 }
+      @Action(Action1, Action2)
+      public test1(state: ITestState) {
+        return {
+          sum: state.sum + 1,
+        }
+      }
+      @Action(Action2)
+      public test2(state: ITestState, draft: ITestState, action: Action2) {
+        draft.sum += action.payload
+      }
+    }
+
+    const testReducer = Test.create()
+    const res1 = testReducer(undefined, new Action1())
+    expect(res1).toEqual({
+      sum: 11,
+    })
+    const res2 = testReducer(undefined, new Action2(3))
+    expect(res2).toEqual({
+      sum: 14,
+    })
   })
 })
